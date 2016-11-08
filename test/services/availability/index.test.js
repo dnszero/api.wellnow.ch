6a9fd59097463c0b4585e8379d93ef3165ac6d6d
Lionel Rudaz <lionelrudaz@icloud.com>
2016-11-08 15:40:09 +0100

    Fix test

--- test/services/availability/index.test.js
+++ test/services/availability/index.test.js
@@ -5,6 +5,6 @@ const app = require('../../../src/app');
 
 describe('availability service', function() {
   it('registered the availabilities service', () => {
-    assert.ok(app.service('availabilities'));
+    assert.ok(app.service('/api/v1/doctors/:doctor_id/availabilities'));
   });
 });
