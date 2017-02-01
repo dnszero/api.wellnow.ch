b6527e80bf2c4f0c0b4eb9b94e91a93edbc20093
Lionel Rudaz <lionelrudaz@icloud.com>
2017-02-01 22:16:12 +0100

    fix block-level variable

--- src/verifier.js
+++ src/verifier.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Verifier } = require('feathers-authentication-local');
+const Verifier = require('feathers-authentication-local').Verifier;
 const bcryptjs = require('bcryptjs');
 
 class WellnowVerifier extends Verifier {
